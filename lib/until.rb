7a5db68ee976bc7f63a4cc02bf52894f5060d43d
Caleb Watters <caleb@h2os.org>
2019-04-04 06:49:27 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,12 @@
 def using_until
+  spell = ""
   levitation_force = 6
-  #your code here
-    
+	until levitation_force == 10
+	 spell += "Wingardium Leviosa\n"
+	levitation_force += 1
+	end
+	puts spell
 end
+
+using_until
 
